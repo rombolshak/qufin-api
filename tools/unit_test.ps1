e230166d67548b756812f3e2a09669d33a24628d
Roman Bolshakov <rombolshak@gmail.com>
2018-05-25 00:38:39 +0300

    try fix tests start

--- tools/unit_test.ps1
+++ tools/unit_test.ps1
@@ -2,4 +2,4 @@ $sciptDirectory = Split-Path $MyInvocation.MyCommand.Path
 $tests = gci -Path "$sciptDirectory\..\src" -Filter "*Tests.csproj" -Recurse |% { $_.FullName }
 Write-Host These projects contains tests: $tests
 # & "$env:xunit20\xunit.console" $tests -xml $sciptDirectory\..\TestResult.xml -appveyor
-dotnet test $tests --no-build -l "trx;LogFileName=$sciptDirectory\..\tests.trx"
+$tests |% { dotnet test $_ --no-build -l "trx;LogFileName=$sciptDirectory\..\tests\$_.trx" }
