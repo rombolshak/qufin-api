aea204fcf9d3c22db2306bd771efd2a745cafa6b
Roman Bolshakov <rombolshak@gmail.com>
2018-05-26 19:35:38 +0300

    several reports are not supported by coveralls

--- tools/unit_test.ps1
+++ tools/unit_test.ps1
@@ -1,6 +1,5 @@
-param([switch]$integration)
 $sciptDirectory = Split-Path $MyInvocation.MyCommand.Path
-$tests = gci -Path "$sciptDirectory\..\src" -Filter "*Tests.csproj" -Recurse |% { $_.FullName } |? { $result = $_.Contains("Integration.Tests"); return -not ($integration -xor $result) }
+$tests = gci -Path "$sciptDirectory\..\src" -Filter "*Tests.csproj" -Recurse |% { $_.FullName }
 Write-Host These projects contains tests: $tests
 $tests |% { dotnet test $_ --no-build -l "trx" }
 exit $LastExitCode 
