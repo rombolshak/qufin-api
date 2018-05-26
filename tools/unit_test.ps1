1e7179d0aab4871e14cc9770654090404fbafa72
Roman Bolshakov <rombolshak@gmail.com>
2018-05-26 19:23:52 +0300

    split unit & integration tests

--- tools/unit_test.ps1
+++ tools/unit_test.ps1
@@ -1,5 +1,6 @@
+param([switch]$integration)
 $sciptDirectory = Split-Path $MyInvocation.MyCommand.Path
-$tests = gci -Path "$sciptDirectory\..\src" -Filter "*Tests.csproj" -Recurse |% { $_.FullName }
+$tests = gci -Path "$sciptDirectory\..\src" -Filter "*Tests.csproj" -Recurse |% { $_.FullName } |? { $result = $_.Contains("Integration.Tests"); return -not ($integration -xor $result) }
 Write-Host These projects contains tests: $tests
 $tests |% { dotnet test $_ --no-build -l "trx" }
 exit $LastExitCode 
