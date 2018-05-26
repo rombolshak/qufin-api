6d9cf5e9c822ff71abd4be9e4d1d5f0980ceff5f
Roman Bolshakov <rombolshak@gmail.com>
2018-05-26 19:01:26 +0300

    fix opencover  .net core support

--- tools/unit_test.ps1
+++ tools/unit_test.ps1
@@ -1,6 +1,5 @@
 $sciptDirectory = Split-Path $MyInvocation.MyCommand.Path
 $tests = gci -Path "$sciptDirectory\..\src" -Filter "*Tests.csproj" -Recurse |% { $_.FullName }
 Write-Host These projects contains tests: $tests
-# & "$env:xunit20\xunit.console" $tests -xml $sciptDirectory\..\TestResult.xml -appveyor
-$tests |% { dotnet test $_ --no-build -l "trx;LogFileName=$sciptDirectory\..\tests\$_.trx" }
+$tests |% { dotnet test $_ --no-build -l "trx" }
 exit $LastExitCode 
